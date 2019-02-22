4312b2bf5f38d31b66d7a6854fa0cad05369aee7
marcus110379 <marcusjmadden@gmail.com>
2019-02-22 12:25:30 -0600

    DONE

--- cupProject/cupApp/urls.py
+++ cupProject/cupApp/urls.py
@@ -3,5 +3,16 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    # "hello" route that takes a name in the URL and returns a response with "hello [NAME]".
+    path('hello/<str:name>/', views.yourName, name="yourName"),
+
+    # "times2" route that takes an argument in the URL and returns a response with "The product times 2 is: [ANSWER]"
+    path('times2/<int:number>/', views.theNumb, name="theNumb"),
+
+    # "total" route that takes an integer in the URL and returns a response with "The sum of all numbers from 0 to the integer is: [SUM]"
+    path('total/<int:number>/', views.theNumb2, name="theNumb2"),
+
+    # 'all/' endpoint that prints out all entry purchaseTimes
+    path('all/', views.all, name="all"),
 ]
--- cupProject/cupApp/views.py
+++ cupProject/cupApp/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Cup
 
 # Create your views here.
 def index(request):
     return HttpResponse("Test URL")
+
+# Function to print hello + the name
+def yourName(request, name):
+    return HttpResponse("Hello "+str(name))
+
+# Function to print the product times
+def theNumb(request,number):
+    return HttpResponse("The product times2 is: "+str(number))
+
+# Function to print the sum of all numbers from 0 to the integer
+def theNumb2(request,number):
+    return HttpResponse("The sum of all numbers from 0 to the integer is: "+str(number))
+
+# Function to print all entry purchaseTimes
+def all(request):
+    allPurchaseTimes = Cup.objects.all()
+    after2012 = Cup.objects.filter(manufactuerDate__gt="2012-01-01")
+
+    context = {
+        'allPurchaseTimes': allPurchaseTimes,
+        'after2012': after2012,
+    }
+
+    return render(request, 'cupApp/index.html', context)
+
